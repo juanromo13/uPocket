d60b0ac589c5ad5f32f13c7519c65ca9fa304db3
juanromo13 <juanochiro1315@gmail.com>
2020-09-23 20:04:20 -0500

    Initial commit

---
+++ app/src/main/java/com/aplimovil/upocket/MainActivity.java
@@ -0,0 +1,15 @@
+package com.aplimovil.upocket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
